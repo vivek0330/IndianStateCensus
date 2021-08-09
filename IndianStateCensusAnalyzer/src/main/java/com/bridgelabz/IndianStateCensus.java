0aa03ca68ee0e59afeced16fe881788415328be4
vivek <vivek.iitd93@gmail.com>
2021-08-09 21:03:19 +0530

    initial Comment

---
+++ IndianStateCensusAnalyzer/src/main/java/com/bridgelabz/IndianStateCensus.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class IndianStateCensus {
+    public static void main(String[] args) {
+        System.out.println("Welcome indian state census problem");
+    }
+}
