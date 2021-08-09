638a4dab85182031f0fe659d9cbc6b162279e911
vivek <vivek.iitd93@gmail.com>
2021-08-09 22:39:08 +0530

    Test cases and exception to read file

--- IndianStateCensusAnalyzer/src/main/java/com/bridgelabz/IndianStateCensus.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz;
-
-public class IndianStateCensus {
-    public static void main(String[] args) {
-        System.out.println("Welcome indian state census problem");
-    }
-}
